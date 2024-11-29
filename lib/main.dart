49f610fc6b5203b779f5a36bc2d818899875f426
bryanmichaelk <bryanmichaelk@gmail.com>
2024-11-30 00:23:00 +0700

    feat: firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 // ignore_for_file: unused_import
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shopify_shopping_list_app/firebase_options.dart';
 import 'routes/routes_app.dart';
 import '../screens/onboarding_page1.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
