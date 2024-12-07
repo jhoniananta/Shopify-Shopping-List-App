56d9339660a94864b2310f9eddedbe6fdcc73e2c
bryanmichaelk <bryanmichaelk@gmail.com>
2024-12-07 13:55:32 +0700

    feat: create list controller

--- lib/models/list.dart
+++ lib/models/list.dart
@@ -1,6 +1,45 @@
-class List {
-  final String id;
-  final String name;
+class ListItem {
+  final String? id;
+  final String title;
+  final String category;
+  final Map<String, Item> items;
 
-  List({required this.id, required this.name});
+  ListItem(
+      {this.id,
+      required this.title,
+      required this.category,
+      required this.items});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'category': category,
+      'items': items.map((key, value) => MapEntry(key, value.toMap())),
+    };
+  }
+}
+
+class Item {
+  String id;
+  String name;
+  String quantity;
+  String unit;
+  bool isDone;
+
+  Item({
+    required this.id,
+    required this.name,
+    required this.quantity,
+    required this.unit,
+    required this.isDone,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'quantity': quantity,
+      'unit': unit,
+      'isDone': isDone,
+    };
+  }
 }
