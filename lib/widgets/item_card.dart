d2151df9d3387cbbf29be9c8ac407fd7c3a1c711
github_darrellv14 <darrell.valentino14@gmail.com>
2024-12-10 21:13:44 +0700

    feat: "ubah menjadi keren dan anti majapahit"

--- lib/widgets/item_card.dart
+++ lib/widgets/item_card.dart
@@ -1,7 +1,4 @@
-// ignore_for_file: prefer_final_fields
-
 import 'package:flutter/material.dart';
-import 'package:shopify_shopping_list_app/models/list.dart';
 import 'package:shopify_shopping_list_app/services/firestore.dart';
 
 class ItemCard extends StatefulWidget {
@@ -12,7 +9,6 @@ class ItemCard extends StatefulWidget {
   final Function(bool?) onCheckboxChanged;
   final VoidCallback? onDeletePressed;
 
-  
   const ItemCard({
     super.key,
     required this.item,
@@ -22,53 +18,105 @@ class ItemCard extends StatefulWidget {
     required this.onCheckboxChanged,
     this.onDeletePressed,
   });
+
   @override
   State<ItemCard> createState() => _ItemCardState();
 }
 
 class _ItemCardState extends State<ItemCard> {
   FirestoreService _firestoreService = FirestoreService();
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(10.0),
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 14.0),
       decoration: BoxDecoration(
-          border: Border.all(
-            width: 1,
-            color: Colors.grey[200]!,
+        gradient: const LinearGradient(
+          colors: [
+            Color(0xFFEFEAFF),
+            Color(0xFFCBC3E3),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(18.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.purple.withOpacity(0.2),
+            blurRadius: 12,
+            spreadRadius: 3,
+            offset: const Offset(0, 6),
           ),
-          borderRadius: BorderRadius.circular(10.0),
-          color: Colors.grey[50]),
+        ],
+      ),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Row(
             children: [
-              Checkbox(value: widget.isDone, onChanged: widget.onCheckboxChanged),
+              // Animated Checkbox
+              GestureDetector(
+                onTap: () => widget.onCheckboxChanged(!widget.isDone),
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                  height: 28,
+                  width: 28,
+                  decoration: BoxDecoration(
+                    color: widget.isDone ? const Color(0xFF7F56D9) : Colors.transparent,
+                    borderRadius: BorderRadius.circular(8.0),
+                    border: Border.all(
+                      color: widget.isDone ? const Color(0xFF7F56D9) : Colors.grey,
+                      width: 2,
+                    ),
+                  ),
+                  child: widget.isDone
+                      ? const Icon(
+                          Icons.check,
+                          color: Colors.white,
+                          size: 18,
+                        )
+                      : null,
+                ),
+              ),
+              const SizedBox(width: 16),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(widget.item,
-                      style: const TextStyle(
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.w600,
-                      )),
-                  Text('${widget.quantity} ${widget.unit}',
-                      style: const TextStyle(
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.w400,
-                      )),
+                  // Item Name
+                  Text(
+                    widget.item,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: widget.isDone ? Colors.grey[600] : Colors.black,
+                      decoration: widget.isDone
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                    ),
+                  ),
+                  const SizedBox(height: 6),
+                  // Quantity and Unit
+                  Text(
+                    '${widget.quantity} ${widget.unit}',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: widget.isDone ? Colors.grey[500] : Colors.grey[700],
+                    ),
+                  ),
                 ],
-              )
+              ),
             ],
           ),
+          // Delete Button
           IconButton(
-            icon: const Icon(
-              Icons.delete, 
-              color: Colors.red
-            ),
+            icon: const Icon(Icons.delete),
+            color: Colors.redAccent,
+            splashRadius: 28,
             onPressed: widget.onDeletePressed,
-          )
+          ),
         ],
       ),
     );
--- lib/widgets/list_card.dart
+++ lib/widgets/list_card.dart
@@ -19,73 +19,101 @@ class CardShoppingList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(12.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
       child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-        elevation: 2,
-        child: SizedBox(
-          height: 165,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+        elevation: 8,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(24),
+            gradient: LinearGradient(
+              colors: [Color(0xFF7F56D9), Color(0xFFB983FF)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.purple.withOpacity(0.2),
+                blurRadius: 12,
+                spreadRadius: 3,
+                offset: const Offset(0, 6),
+              ),
+            ],
+          ),
           child: Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: const EdgeInsets.all(20.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                // Header Row: Title and Delete Button
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
                       title,
                       style: const TextStyle(
-                        fontSize: 18,
+                        fontSize: 22,
                         fontWeight: FontWeight.bold,
+                        color: Colors.white,
                       ),
                     ),
                     IconButton(
-                      icon: const Icon(
-                        Icons.delete,
-                        color: Color(0xFF7F56D9),
-                      ),
+                      icon: const Icon(Icons.delete, color: Colors.white70),
                       onPressed: onDeletePressed,
+                      splashRadius: 28,
                     ),
                   ],
                 ),
-                const SizedBox(height: 8),
+                const SizedBox(height: 16),
+                // Avatar and Subtitle
                 Row(
                   children: [
                     CircleAvatar(
-                      radius: 20,
+                      radius: 30,
+                      backgroundColor: Colors.white.withOpacity(0.2),
                       backgroundImage: AssetImage(userImages),
                     ),
-                    const SizedBox(width: 8),
+                    const SizedBox(width: 16),
                     Expanded(
                       child: Text(
                         subtitle,
-                        style: const TextStyle(color: Colors.grey),
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.white70,
+                          fontWeight: FontWeight.w400,
+                        ),
                       ),
                     ),
                   ],
                 ),
-                const SizedBox(height: 8),
+                const SizedBox(height: 20),
+                // Footer Row: Tag and Additional Info
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Row(
                       children: [
-                        const Icon(Icons.list, size: 16, color: Colors.grey),
-                        const SizedBox(width: 4),
+                        const Icon(Icons.list, size: 20, color: Colors.white70),
+                        const SizedBox(width: 6),
                         Text(
                           subtitle,
-                          style: const TextStyle(color: Colors.grey),
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.white70,
+                          ),
                         ),
                       ],
                     ),
                     Row(
                       children: [
-                        const Icon(Icons.label, size: 16, color: Colors.grey),
-                        const SizedBox(width: 4),
+                        const Icon(Icons.label, size: 20, color: Colors.white70),
+                        const SizedBox(width: 6),
                         Text(
                           tag,
-                          style: const TextStyle(color: Colors.grey),
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.white70,
+                          ),
                         ),
                       ],
                     ),
